b42433eba15cba8a0e5217d7c9b65c0a2ecfc479
hazem hadda <hazemhadda2002@gmail.com>
2026-02-23 12:04:49 +0100

    tp3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
+import { HomeComponent } from './core/home/home.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
 
 
 @NgModule({
@@ -15,6 +17,8 @@ import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.
     HeaderComponent,
     FooterComponent,
     ListSuggestionComponent,
+    HomeComponent,
+    NotfoundComponent,
 
   ],
   imports: [
