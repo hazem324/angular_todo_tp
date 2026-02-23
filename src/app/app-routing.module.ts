8a26d3a88c719d0624a34e95358eaa5e2be4c4a0
hazem hadda <hazemhadda2002@gmail.com>
2026-02-23 13:46:24 +0100

    simple routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'Home', pathMatch: 'full'}, 
+  {path:'home', component: HomeComponent},
+{ path: 'listSuggestion', component: ListSuggestionComponent },
+  {path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
